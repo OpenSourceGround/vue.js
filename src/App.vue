69ca47dac3eea5b1786185ed11429d0447f8f175
Jareer Abdullah <jforeverything2007@gmail.com>
2022-07-11 15:14:21 +0200

    Update

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/index.scss";
+@import "@/assets/tailwinds.scss";
+</style>
